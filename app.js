1ce1e585f597920ff83aa63e16e59979ee6f8722
Omar Fahmy <Ofahmy1234@gmail.com>
2022-09-01 00:52:47 +0200

    Add ejs to our project

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const app = express();
 app.use(express.urlencoded({extended:true}));
 
 app.listen("3000",function(){
+
     console.log("Server running on port 3000")
 })
 
 app.get("/",function(req,res){
-    res.send("yelloooo");
+    var today = new Date();
+    if(today.getDay() === 3 || today.getDay()===0 ){
+        res.send("yay it's the weekend");
+    }else{
+        res.send("I have to go to work!!");
+    }
 })
