0e96e3c7b7d018c90cfce30d750c127715c4311b
Omar Fahmy <Ofahmy1234@gmail.com>
2022-09-01 05:06:45 +0200

    Add getting the current day as a header feature

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.urlencoded({extended:true}));
+app.set('view engine','ejs');
 
 app.listen("3000",function(){
 
@@ -8,10 +9,40 @@ app.listen("3000",function(){
 })
 
 app.get("/",function(req,res){
-    var today = new Date();
-    if(today.getDay() === 3 || today.getDay()===0 ){
-        res.send("yay it's the weekend");
-    }else{
-        res.send("I have to go to work!!");
+    var today = new Date().getDay();
+    var day= null;
+    var obj = {
+        name: "fahmy",
+        age:"21",
+        gender:"male"
+    };
+
+    switch(today){
+        case 0:
+            day="Sunday";
+            break;
+        case 1:
+            day="Monday";
+            break;
+        case 2:
+            day="Tuesday";
+            break;
+        case 3:
+            day="Wednesday";
+            break;
+        case 4:
+            day="Thursday";
+            break;
+        case 5:
+            day="Friday";
+            break;
+        case 6:
+            day="Saturday";
+            break;
+        default:
+            console.log("Error current day not from 0 ~ 6")
+            break;
+                    
     }
+    res.render("list",{obj:obj});
 })
