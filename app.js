075a9b0830a6b5f5f275e11d3de95bd528e7ab0a
Omar Fahmy <Ofahmy1234@gmail.com>
2022-09-15 19:01:25 +0200

    Add deletion button for custom lists and all lists list

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ const listSchema = mongoose.Schema({
 });
 const list = mongoose.model("list",listSchema);
 
-app.get("/:listName",function(req,res){
+app.get("/lists/:listName",function(req,res){
     let requestedList = req.params.listName;
     console.log(requestedList);
     list.find({name:requestedList},function(err,foundList){
@@ -120,21 +120,64 @@ app.post("/",function(req,res){
                         console.log(err);
                     })
                 
-        res.redirect("/"+currentURL);
+        res.redirect("/lists/"+currentURL);
 
     }
 
 });
 app.post("/delete",function(req,res){
-    console.log(req.body.checkbox);
-    homeList.findByIdAndRemove(req.body.checkbox,function(err){
-        if(err) console.log("Error in deletion of item")
-    })
-    res.redirect("/");
-})
+    const arr = req.body.checkbox.split(",")
+    const listHeader = arr[0];
+    const item_id = arr[1];
+    if(listHeader === date.getDay()){
+        homeList.findByIdAndRemove(item_id,function(err){
+            if(err) {console.log("Error in deletion of item first one");
+        console.log(err)}
+        })
+        res.redirect("/");
+    }else if(listHeader === "lists"){
+
+        list.findByIdAndRemove(item_id,function(err){
+            if(err) console.log(err);
+        })
+        res.redirect("/lists")
+
+
+    }else{
+        list.find({name:listHeader},function(err,foundLists){
+            const itemsArr = foundLists[0].items
+
+            itemsArr.forEach(function(item) {
+                if(item_id === item.id){
+
+                    list.updateOne({name:listHeader},{$pull: {items:item} },function(err){
+                        if(err) console.log(err);
+                    });
+                }else{
+
+                }
+            });
+            })
+            res.redirect("/lists/"+listHeader);
+        }
+    }
+
+)
 
 
 
 app.get("/about",function(req,res){
     res.render("about");
+})
+
+app.post("/lists",function(req,res){
+    res.redirect("/lists");
+})
+
+app.get("/lists",function(req,res){
+    list.find(function(err,foundLists){
+        // res.send(foundLists)
+        res.render("list",{day:"lists" , items:foundLists});
+
+    })
 })
